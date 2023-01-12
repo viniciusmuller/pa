5da117626024e47ccb2ac7d779d3f654267fd112
Vinícius Müller <vinigm.nho@gmail.com>
2023-01-12 09:02:37 -0300

    feat: support adding and deleting aliases

--- cmd/alias.go
+++ cmd/alias.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -12,21 +13,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type PaDataAliases struct {
-	Name    string
-	Command string
+type PaAlias struct {
+	Name    string `json:"name"`
+	Command string `json:"command"`
+}
+
+type PaProject struct {
+	Aliases []PaAlias `json:"aliases"`
 }
 
 type PaData struct {
-	Aliases []PaDataAliases
+	Projects map[string]PaProject
 }
 
 var alias string
 var command string
+var deleteAlias bool
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
 	versionCmd.PersistentFlags().StringVarP(&alias, "alias", "a", "", "the alias to use")
+	versionCmd.PersistentFlags().BoolVarP(&deleteAlias, "delete", "d", false, "whether to delete the alias")
 	versionCmd.PersistentFlags().StringVarP(&command, "command", "c", "", "the command to run")
 }
 
@@ -43,19 +50,72 @@ var versionCmd = &cobra.Command{
 			}
 		}
 
-		// TODO: Read config file, update it based on user input and write it
-		// back again
-		var configFile = path.Join(dataDirectory, "data.json")
-		data := PaData{
-			Aliases: []PaDataAliases{
-				{
-					Name:    "test",
-					Command: "mix test",
-				},
-			},
+		currentDir, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("error getting current directory: %s", err)
 		}
 
+		var dataFilePath = path.Join(dataDirectory, "data.json")
+		content, err := ioutil.ReadFile(dataFilePath)
+		if err != nil {
+			log.Fatalf("error opening data file: %s", err)
+		}
+
+		var data PaData
+		err = json.Unmarshal(content, &data)
+		if err != nil {
+			log.Fatalf("error parsing data file: %s", err)
+		}
+
+		project, ok := data.Projects[currentDir]
+		if !ok {
+			data.Projects = make(map[string]PaProject)
+			project = PaProject{}
+			data.Projects[currentDir] = project
+		}
+
+		if !deleteAlias {
+			alias := PaAlias{
+				Name:    alias,
+				Command: command,
+			}
+
+			project, err = addProjectAlias(project, alias)
+			if err != nil {
+				log.Fatalf("could not add alias to project: %s", err)
+			}
+		}
+
+		if deleteAlias {
+			project = deleteProjectAlias(project, alias)
+		}
+
+		data.Projects[currentDir] = project
+
 		file, _ := json.MarshalIndent(data, "", "  ")
-		_ = ioutil.WriteFile(configFile, file, 0644)
+		_ = ioutil.WriteFile(dataFilePath, file, 0644)
 	},
+}
+
+func addProjectAlias(project PaProject, alias PaAlias) (PaProject, error) {
+	for _, existingAlias := range project.Aliases {
+		if alias.Name == existingAlias.Name {
+			return PaProject{}, fmt.Errorf("alias '%s' already exists in project", alias.Name)
+		}
+	}
+
+	project.Aliases = append(project.Aliases, alias)
+	return project, nil
+}
+
+func deleteProjectAlias(project PaProject, targetAlias string) PaProject {
+	var filteredAliases []PaAlias
+	for _, alias := range project.Aliases {
+		if alias.Name != targetAlias {
+			filteredAliases = append(filteredAliases, alias)
+		}
+	}
+
+	project.Aliases = filteredAliases
+	return project
 }
